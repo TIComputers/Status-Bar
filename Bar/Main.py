0b934d81cf2ac0a98e2e40126cc287bdbc87ed77
TIComputer <Computer_CoDism@proton.me>
2025-11-23 14:58:47 +0330

    Create Main.py

---
+++ Bar/Main.py
@@ -0,0 +1,89 @@
+from time import time
+
+
+class Bar:
+    def __init__(self):
+        self.block  = 30
+        self.total  = 0.0
+        self.filled = 0.0 
+        
+        self.times  = 0.0
+        self.timef  = 0.0
+        self.sumtime= 0.0
+        self.checkt = False
+        self.checkb = True
+        self.text   = ""
+        
+        self.white  = "\033[97m"
+        self.red    = "\033[91m"
+        self.yellow = "\033[93m"
+        self.green  = "\033[92m"
+        self.gray   = "\033[90m"
+        self.reset  = "\033[0m"
+            
+        self.shap1  = "▄"
+        self.shap2  = "▓"
+        self.shap3  = "█"
+        self.shap4  = "▐"
+    
+    
+    def color_bar(self, value=0):
+        if value < 50:
+            color_bar = self.white
+        elif 50 <=  value < 85:
+            color_bar = self.yellow
+        elif 85 <=  value < 100.00:
+            color_bar = self.red
+        elif value == 100.00:
+            color_bar = self.green
+        else: color_bar = self.green
+        return color_bar
+        
+    def calculate_block(self, precent=0):
+        calculate = int((precent * self.block) / 100)
+        remaining = self.block - calculate
+        return [calculate, remaining]
+        
+    def display_bar(self, precentage=0):
+        sumt  = self.sumtime
+        color = self.color_bar(precentage)
+        block = self.calculate_block(precentage)
+        done  = block[1]
+        ndone = block[0]
+        reset = self.reset
+        shap0 = self.shap4
+        chline= ""
+
+        if self.checkb == True:
+            print(f"\r{color}{shap0*ndone}",end="")
+            print(f"{self.gray}{shap0*done}   ",end="")
+        else: chline = "\r"
+        print(f"{chline}{reset}[{color}%{precentage:.2f}{reset}]  ",end="")
+        if self.checkt == True:
+            print(f"{color}{sumt:.2f}sec{self.reset}", end="")
+        print(f"  {self.yellow}{self.text}{reset}", end="")
+
+
+    def calculate_time(self):
+        calculate    = (self.timef - self.times) / self.filled
+        process      = self.total - self.filled
+        self.sumtime = calculate * process
+        
+    def get_time(self):
+        self.times  = float(time())
+        self.checkt = True
+        
+    def update(self):
+        self.calculate_time()
+        precent = (self.filled * 100) / self.total
+        self.display_bar(precent)  
+    
+    def get_process_processed(self, process=0, processd=0, text="", check_bar=True):
+        self.timef = float(time())
+        self.checkb= check_bar
+        self.total = process
+        self.filled= processd + 1
+        self.text  = text
+        self.update()
+        
+        
